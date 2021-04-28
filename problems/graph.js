705678788032ed4844247f00889e13f55d8580d8
Huy Lam <commanderh1995@gmail.com>
2021-04-28 12:55:47 -0500

    solve addVertex

--- problems/graph.js
+++ problems/graph.js
@@ -1,37 +1,43 @@
 
 class Graph {
-  constructor() {
-    // Code goes here ...
-  }
+    constructor() {
+        this.adjList = {};
+    }
 
-  addVertex(vertex) {
-    // Code goes here ...
-  }
+    addVertex(vertex) {
+        if(!this.adjList[vertex]) {
+            this.adjList[vertex] = [];
+        }
+    }
 
-  addEdges(srcValue, destValue) {
-    // Code goes here ...
-  }
+    addEdges(srcValue, destValue) {
+        if(!this.adjList[srcValue]) this.addVertex(srcValue);
+        if(!this.adjList[destValue]) this.addVertex(destValue);
+        console.log(this.adjList);
+        // if(!this.adjList[srcValue]) this.adjList[srcValue].push(destValue);
+        // if(!this.adjList[destValue]) this.adjList[destValue].push(srcValue);
+    }
 
-  buildGraph(edges) {
-    // Code goes here ...
-  }
+    buildGraph(edges) {
+        // Code goes here ...
+    }
 
-  breadthFirstTraversal(startingVertex) {
-    // Code goes here ...
-  }
+    breadthFirstTraversal(startingVertex) {
+        // Code goes here ...
+    }
 
-  depthFirstTraversalIterative(startingVertex) {
-    // Code goes here ...
-  }
+    depthFirstTraversalIterative(startingVertex) {
+        // Code goes here ...
+    }
 
-  depthFirstTraversalRecursive(startingVertex, visited = new Set(), vertices = []) {
-    // Code goes here ...
-  }
+    depthFirstTraversalRecursive(startingVertex, visited = new Set(), vertices = []) {
+        // Code goes here ...
+    }
 
 }
 
 module.exports = {
-  Graph
+    Graph
 };
 
 
